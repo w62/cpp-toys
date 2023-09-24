ac37b7a3c09bfafcf67cd18666e4cc12e47c76b1
william wong <william@freepackets.net>
2023-09-24 15:02:22 +0800

    Update README.md

--- main.cpp
+++ main.cpp
@@ -1,18 +1,15 @@
 #include <iostream>
 
-void add2(int x)
-{
-	x += 2;
-	std::cout << x << '\n';
-
+void add2(int x) {
+  x += 2;
+  std::cout << x << '\n';
 }
 
-int main()
-{
-	int a;
-	a = 4;
-	add2(a); 
-	std::cout << "a: " << a << '\n';
-	
-	return 0;
+int main() {
+  int a;
+  a = 4;
+  add2(a);
+  std::cout << "a: " << a << '\n';
+
+  return 0;
 }
