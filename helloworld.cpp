334df28ba572691a286200de18d3b2cb0f25bd37
william <william@freepackets.net>
2023-09-27 18:00:24 +0800

    Add .clang-format

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,28 +1,35 @@
+#include <array>
 #include <iostream>
+#include <span>
+
+struct Foo
+{
+    int a{ };
+    int b{ };
+    int c{ };
+};
+
+consteval int sum(std::span<const int> a) // std::span and consteval
+{
+    int s{ 0 };
+    for (auto n : a)
+        s += n;
+    return s;
+}
+
+auto sum(auto x, auto y) -> decltype(x + y) // abbreviated function templates
+{
+    return x + y;
+}
 
 int main()
 {
-	int width;
-	width = 5; // copy assignment of value into variable width
-	
-	std::cout << width; // prints 5
-	
-	width = 7; //change value stored in variable width to 7
-	
-	std::cout << width; // prints 7
-						//
+    constexpr std::array a{ 3, 2, 1 };
+    constexpr int s{ sum(a) };
+    std::cout << s << '\n';
 
-	int a;   // no initializer (default initialization)
-	int b = 5; // initializer after equals sign (copy initialization)
-	int c( 6 );  // initializer in parenthesis (direct initialization)
-				 //
+    Foo f1{ .a = 1, .c = 3 }; // designated initializers
+    std::cout << sum(f1.a, f1.c) << '\n';
 
-	// List initialization methods (C++11) (preferred)
-	//
-	
-	int d { 7 }; // initializer in braces (direct list initialization)
-	int e = { 8 }; // initializer in braces after equals sign (copy list initialization)
-	int f {}; // initializer is empty braces (value initialization)
-					   
-	return 0;
+    return 0;
 }
