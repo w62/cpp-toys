2da71afd921dcd4860760e88def96ff10858c5fb
william <william@freepackets.net>
2023-11-02 17:13:20 +0800

    Add sqlite embedded example   Changes to be committed:  	modified:   CMakeLists.txt  	new file:   db/CMakeLists.txt         new file:   db/README.md  	new file:   db/demo.cpp  	new file:   db/sqlite/shell.c  	new file:   db/sqlite/sqlite-amalgamation-3440000.zip  	new file:   db/sqlite/sqlite3.c  	new file:   db/sqlite/sqlite3.h  	new file:   db/sqlite/sqlite3ext.h  	modified:   guineapig/gp.cpp

---
+++ db/README.md
@@ -0,0 +1,2 @@
+A small sqlite embedded example base on 
+[Coding with Chapa](https://www.youtube.com/watch?v=L-hnA82JsEM)
--- guineapig/gp.cpp
+++ guineapig/gp.cpp
@@ -1,41 +1,11 @@
-#include <iostream>
 
-using namespace std;
-
-class Base {
- public:
-  virtual void someMethod();
-
- protected:
-  int mProtectedInt;
-
- private:
-  int mPrivateInt;
+auto l ()
+{
+    int i;
+    return [[i](){}](){};
 };
+int main ()
+{
 
-class Derived : public Base {
- public:
-  virtual void someMethod() override;
-  virtual void someOtherMethod();
-};
-
-int main() {
-  Base* base = new Derived();
-
-  // virtual function binding
-
-  // Non-virtual function binding
-  //
-  base->someOtherMethod();
-  return 0;
+    return 0;
 }
-
-void Base::someMethod() {
-  cout << "This is Base's version of someMethod(). " << endl;
-}
-
-void Derived::someMethod() {
-  cout << "This is Derived's version of someMethod(). " << endl;
-}
-
-void Derived::someOtherMethod() { cout << "SomeOtherMethod " << endl; }
