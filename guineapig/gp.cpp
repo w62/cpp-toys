16fff96f3308320231ff102e277be075fe95b176
william <william@freepackets.net>
2023-11-12 17:55:11 +0800

    Types Alias for Function Pointers
    Do some testings based on Type Alias for Function Pointers on page 353 -
    354 of Professional C++ 4th ed.
     	modified:   gp.cpp

--- guineapig/gp.cpp
+++ guineapig/gp.cpp
@@ -1,11 +1,40 @@
+#include <iostream>
+// Type Alias for Function pointers:
+//
+// Based on page 353 - 354 of professional C++ 4th ed.
+//
+int add(int, int);
+int sub(int, int);
+int mul(int, int);
 
-auto l ()
-{
-    int i;
-    return [[i](){}](){};
-};
-int main ()
-{
+int main() {
+  using IntFunction = int (*)(int, int);
 
-    return 0;
+  IntFunction IntFunc;
+
+  IntFunc = add;
+  std::cout << IntFunc(1, 2) << std::endl;
+
+  IntFunc = sub;
+  std::cout << IntFunc(3, 4) << std::endl;
+
+  IntFunc = mul;
+  std::cout << IntFunc(5, 6) << std::endl;
+
+  return 0;
+}
+
+int add(int a, int b) {
+  std::cout << "add" << std::endl;
+  return a + b;
+}
+
+int sub(int a, int b) {
+  std::cout << "sub" << std::endl;
+  return a - b;
+}
+
+int mul(int a, int b) {
+  std::cout << "mux" << std::endl;
+  return a * b;
 }
