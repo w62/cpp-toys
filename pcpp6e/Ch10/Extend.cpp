e364e73817dd674de205295b32f6b3d05a5956e9
william <william@freepackets.net>
2025-10-03 07:17:33 +0800

    Working on chapter 10

--- pcpp6e/Ch10/Extend.cpp
+++ pcpp6e/Ch10/Extend.cpp
@@ -1,32 +1,31 @@
-#include <print>
+%:include <print>
 
 using namespace std;
 class Base {
-  public:
-  void someFunction() { /* ... */ }
-  protected:
-  int m_protectedInt {0};
-  private:
-  int m_privateInt {0};
+ public:
+  void someFunction() {
+    println("someFunction called");
+  /* ... */ }
+
+ protected:
+  int m_protectedInt{0};
+
+ private:
+  int m_privateInt{0};
 };
 
-class Derived : public Base {
-  public:
-  void anotherFunction() {
-    someFunction();      // OK: can access public member of Base
-    m_protectedInt = 42; // OK: can access protected member of Base
-    // m_privateInt = 42; // Error: cannot access private member of Base
-  }
-  void printProtected() {
-   println("{}", m_protectedInt);
-  }
+class Derived : public Base <%
+ public:
+  void someOtherFunction() { println("someOtherFunction called"); }
+  void printProtected() { println("{}", m_protectedInt); }
 };
 
-int main() {
+int main() <%
   Derived d;
-  d.someFunction();      // OK: can access public member of Base through Derived
+  d.someFunction();  // OK: can access public member of Base through Derived
   // d.m_protectedInt = 42; // Error: cannot access protected member of Base
   // d.m_privateInt = 42;   // Error: cannot access private member of Base
-  d.printProtected (); 
+  d.someOtherFunction();
+  d.printProtected();
   return 0;
-}
+%>
